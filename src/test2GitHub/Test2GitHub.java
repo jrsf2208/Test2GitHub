ac8ae7fcec92c480fcb67a8db30c7dce818fe218
horn3t <jrsf2208@gmail.com>
2015-05-24 23:31:15 -0500

    Initial Commit

---
+++ src/test2GitHub/Test2GitHub.java
@@ -0,0 +1,11 @@
+package test2GitHub;
+
+/**
+ * Created by Ritzman on 5/24/2015.
+ */
+public class Test2GitHub {
+
+    public static void main (String args[]){
+        System.out.println("Hi");
+    }
+}
